773f41e66eaf3178861cdb46600d6b0aec9ecac1
jmrickard <jonny.rickard@gmail.com>
2018-01-21 10:33:56 -0600

    Updated deploy.sh and added undercloud.conf

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 openstack overcloud deploy --templates \
+-e /home/stack/templates/puppet-ceph-external.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /home/stack/templates/network-environment.yaml \
 --compute-scale 1 \
 --control-scale 1 \
+--compute-flavor compute \
+--control-flavor control \
 --ntp-server 10.1.1.253
